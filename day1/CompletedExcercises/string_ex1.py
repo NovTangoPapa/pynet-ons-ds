a8255d1da84b0158dddc401b51191024a25fd28f
Nick Iriarte <nick.iriarte@docusign.com>
2019-06-20 13:51:50 -0700

    Adding string excercise 1

---
+++ day1/CompletedExcercises/string_ex1.py
@@ -0,0 +1,7 @@
+name_01 = "Nick Dog"
+name_02 = "Nick Cat"
+name_03 = "Nick Hamster"
+name_04 = input("Enter your Nick Name: ")
+
+print("{:30}{:30}{:30}{:30}".format(name_01, name_02, name_03, name_04))
+
